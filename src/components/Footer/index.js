a78584ebcb276ec63006921552d7e1f0e4333039
Googleplex <yyoung2001@gmail.com>
2021-03-10 14:10:09 +0800

    edit: Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,20 @@
 import { Layout } from "antd";
+import styles from "./index.less";
 
 const Footer = () => {
-  return <Layout.Footer>&copy; Copyright 2021 Googleplex</Layout.Footer>;
+  return (
+    <Layout.Footer style={{ textAlign: "center" }}>
+      &copy; Copyright 2021{" "}
+      <a
+        href="https://github.com/y-young"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.footerLink}
+      >
+        Googleplex
+      </a>
+    </Layout.Footer>
+  );
 };
 
 export default Footer;

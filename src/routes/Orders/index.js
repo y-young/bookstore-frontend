f3be9ea63286112f329952a34f4f593d3c910fd0
Googleplex <yyoung2001@gmail.com>
2021-03-12 13:45:36 +0800

    feat: order statistics

--- src/routes/Orders/index.js
+++ src/routes/Orders/index.js
@@ -10,6 +10,7 @@ import {
   List,
   Row,
   Space,
+  Statistic,
   Typography,
 } from "antd";
 import { useHistory } from "react-router-dom";
@@ -36,66 +37,84 @@ const Orders = () => {
         </Col>
       </Row>
       <Divider />
-      <List
-        itemLayout="horizontal"
-        dataSource={items}
-        footer={
-          <>
-            {items.length > 0 && (
-              <Row justify="space-between">
-                <p>共 {items.length} 个订单</p>
-              </Row>
-            )}
-          </>
-        }
-        renderItem={(item) => (
-          <List.Item
-            actions={[
-              <Button type="primary" onClick={() => history.push("/order")}>
-                查看详情
-              </Button>,
-            ]}
-            className={styles.orderListItem}
-          >
-            <List.Item.Meta
-              avatar={
-                <Space>
-                  <Image src={item.cover} width={60} height={60} />
-                  <Image src={item.cover} width={60} height={60} />
-                  <Image src={item.cover} width={60} height={60} />
-                  <div className={styles.booksExtra}>+3</div>
-                </Space>
-              }
-              title={"时间：2021-03-11 11:11:11"}
-              description={"编号：11111111111"}
-              className={styles.orderBookItems}
-            />
-            <Col flex={1} className={styles.orderItemsCount}>
-              <>{item.amount ? item.amount : 1} 件商品</>
-            </Col>
-            <Col flex="1 1 50px" className={styles.orderTotal}>
-              {currencyFormat(item.price)}{" "}
-            </Col>
-          </List.Item>
-        )}
-        locale={{
-          emptyText: (
-            <Empty
-              description={
-                <span style={{ fontSize: "26px" }}>您还没有订单</span>
-              }
-            >
-              <Button
-                type="primary"
-                size="large"
-                onClick={() => history.push("/")}
+      <Row gutter={16} className={styles.statistics}>
+        <Col span={8}>
+          <Statistic title="订单数量" value={items.length} />
+        </Col>
+        <Col span={8}>
+          <Statistic title="商品数量" value={11} />
+        </Col>
+        <Col span={8}>
+          <Statistic title="总金额" value={1128} precision={2} />
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={20}>
+          <List
+            itemLayout="horizontal"
+            dataSource={items}
+            renderItem={(item) => (
+              <List.Item
+                actions={[
+                  <Button type="primary" onClick={() => history.push("/order")}>
+                    查看详情
+                  </Button>,
+                ]}
+                className={styles.orderListItem}
               >
-                去购物
-              </Button>
-            </Empty>
-          ),
-        }}
-      />
+                <List.Item.Meta
+                  avatar={
+                    <Space>
+                      <Image src={item.cover} width={60} height={60} />
+                      <Image src={item.cover} width={60} height={60} />
+                      <Image src={item.cover} width={60} height={60} />
+                      <div className={styles.booksExtra}>+3</div>
+                    </Space>
+                  }
+                  title={"时间：2021-03-11 11:11:11"}
+                  description={
+                    String(item.amount ? item.amount : 1) + " 件商品"
+                  }
+                  className={styles.orderBookItems}
+                />
+                <Col flex="1 1 50px" className={styles.orderTotal}>
+                  {currencyFormat(item.price)}{" "}
+                </Col>
+              </List.Item>
+            )}
+            locale={{
+              emptyText: (
+                <Empty
+                  description={
+                    <span style={{ fontSize: "26px" }}>您还没有订单</span>
+                  }
+                >
+                  <Button
+                    type="primary"
+                    size="large"
+                    onClick={() => history.push("/")}
+                  >
+                    去购物
+                  </Button>
+                </Empty>
+              ),
+            }}
+          />
+        </Col>
+        <Col>
+          <Divider type="vertical" className={styles.divider} />
+        </Col>
+        <Col span={3}>
+          <Row gutter={8} className={styles.bookStatistics}>
+            <Col span={18}>外国文学</Col>
+            <Col span={6}>10</Col>
+            <Col span={18}>科幻小说</Col>
+            <Col span={6}>23</Col>
+            <Col span={18}>计算机</Col>
+            <Col span={6}>3</Col>
+          </Row>
+        </Col>
+      </Row>
     </>
   );
 };

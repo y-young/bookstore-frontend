31dc662e864a2ccc0e4088bd74a0c467dd3cc3a1
Googleplex <yyoung2001@gmail.com>
2021-05-10 17:28:29 +0800

    enhance: handle request error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { UseAPIProvider } from "@umijs/use-request";
+import { message } from "antd";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import React from "react";
@@ -6,6 +8,7 @@ import {
   Redirect,
   Route,
   Switch,
+  useHistory,
 } from "react-router-dom";
 import BookDetail from "routes/BookDetail";
 import BookManagement from "routes/BookManagement";
@@ -20,58 +23,93 @@ import Register from "routes/Register";
 import Settings from "routes/Settings";
 import Statistics from "routes/Statistics";
 import UserManagement from "routes/UserManagement";
+import request from "umi-request";
 import styles from "./App.less";
 
 const App = () => {
+  const history = useHistory();
+
   return (
-    <Router>
-      <Header />
-      <main className={styles.main}>
-        <Switch>
-          <Route path="/" exact={true}>
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/books/:bookId">
-            <BookDetail />
-          </Route>
-          <Route path="/books">
-            <Books />
-          </Route>
-          <Route path="/cart">
-            <Cart />
-          </Route>
-          <Route path="/order">
-            <OrderDetail />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Route path="/manage/books">
-            <BookManagement />
-          </Route>
-          <Route path="/manage/users">
-            <UserManagement />
-          </Route>
-          <Route path="/manage/orders">
-            <OrderManagement />
-          </Route>
-          <Route path="/settings">
-            <Settings />
-          </Route>
-          <Redirect from="*" to="/" />
-        </Switch>
-      </main>
-      <Footer />
-    </Router>
+    <UseAPIProvider
+      value={{
+        requestMethod: (param) => {
+          const prefix = "http://localhost:8080/api";
+          const token = localStorage.getItem("bookstore_token");
+          const headers = token ? { Authorization: token } : {};
+          if (typeof param === "string") {
+            return request(param, { prefix, headers });
+          }
+          if (typeof param === "object") {
+            return request(param.url, { ...param, prefix, headers });
+          }
+        },
+        formatResult: (response) => response.data,
+        onError: async (error, params) => {
+          const body = await error.response.json();
+          const { status } = error.response;
+          switch (status) {
+            case 500:
+              message.error("服务器错误");
+              break;
+            case 401:
+              message.warn("请先登录");
+              history.push("/login");
+              break;
+            default:
+              message.error(body.message);
+          }
+        },
+      }}
+    >
+      <Router>
+        <Header />
+        <main className={styles.main}>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/books/:bookId">
+              <BookDetail />
+            </Route>
+            <Route path="/books">
+              <Books />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/order">
+              <OrderDetail />
+            </Route>
+            <Route path="/orders">
+              <Orders />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="/manage/books">
+              <BookManagement />
+            </Route>
+            <Route path="/manage/users">
+              <UserManagement />
+            </Route>
+            <Route path="/manage/orders">
+              <OrderManagement />
+            </Route>
+            <Route path="/settings">
+              <Settings />
+            </Route>
+            <Redirect from="*" to="/" />
+          </Switch>
+        </main>
+        <Footer />
+      </Router>
+    </UseAPIProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { UseAPIProvider } from "@umijs/use-request";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/lib/locale/zh_CN";
 import React from "react";
 import ReactDOM from "react-dom";
-import request from "umi-request";
 import { ProvideAuth } from "utils/useAuth";
 import App from "./App";
 import "./index.less";
@@ -11,28 +9,11 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UseAPIProvider
-      value={{
-        requestMethod: (param) => {
-          const prefix = "http://localhost:8080/api";
-          const token = localStorage.getItem("bookstore_token");
-          const headers = token ? { Authorization: token } : {};
-          if (typeof param === "string") {
-            return request(param, { prefix, headers });
-          }
-          if (typeof param === "object") {
-            return request(param.url, { ...param, prefix, headers });
-          }
-        },
-        formatResult: (response) => response.data,
-      }}
-    >
-      <ConfigProvider locale={zhCN}>
-        <ProvideAuth>
-          <App />
-        </ProvideAuth>
-      </ConfigProvider>
-    </UseAPIProvider>
+    <ConfigProvider locale={zhCN}>
+      <ProvideAuth>
+        <App />
+      </ProvideAuth>
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

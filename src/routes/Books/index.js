8729deac01a5a5c7ea66e6ef2e77f53f4d6c7302
Googleplex <yyoung2001@gmail.com>
2021-05-13 08:22:32 +0800

    feat: simple book search in frontend

--- src/routes/Books/index.js
+++ src/routes/Books/index.js
@@ -2,19 +2,32 @@ import useRequest from "@umijs/use-request";
 import { Input, Spin } from "antd";
 import BookList from "components/BookList";
 import PageHeader from "components/PageHeader";
+import { useState } from "react";
 
 const Books = () => {
   const { data, loading } = useRequest("/books", {
-    initialData: [],
+    onSuccess: (books) => setBooks(books),
   });
+  const [books, setBooks] = useState([]);
+
+  const onSearch = (keyword) => {
+    if (!keyword) {
+      setBooks(data);
+    }
+    setBooks(
+      data.filter(
+        (book) => book.title.includes(keyword) || book.author.includes(keyword)
+      )
+    );
+  };
 
   return (
     <>
       <PageHeader title="书籍" span={8}>
-        <Input.Search placeholder="搜索书籍" />
+        <Input.Search placeholder="搜索书籍" onSearch={onSearch} />
       </PageHeader>
       <Spin spinning={loading}>
-        <BookList books={data} />
+        <BookList books={books} />
       </Spin>
     </>
   );

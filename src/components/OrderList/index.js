e75f5add2e379616447b28c4bb5887bed32bc2f9
Googleplex <yyoung2001@gmail.com>
2021-05-13 10:14:05 +0800

    feat: view my orders

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -3,40 +3,57 @@ import { useHistory } from "react-router-dom";
 import { currencyFormat } from "utils/helpers";
 import styles from "./index.less";
 
-const OrderList = ({ books }) => {
+const OrderList = ({ orders }) => {
   const history = useHistory();
 
   return (
     <List
       itemLayout="horizontal"
-      dataSource={books}
-      renderItem={(book) => (
-        <List.Item
-          actions={[
-            <Button type="primary" onClick={() => history.push("/order")}>
-              查看详情
-            </Button>,
-          ]}
-          className={styles.orderListItem}
-        >
-          <List.Item.Meta
-            avatar={
-              <Space>
-                <Image src={book.cover} width={60} height={60} />
-                <Image src={book.cover} width={60} height={60} />
-                <Image src={book.cover} width={60} height={60} />
-                <div className={styles.booksExtra}>+3</div>
-              </Space>
-            }
-            title={"时间：2021-03-11 11:11:11"}
-            description={String(book.amount ? book.amount : 1) + " 件商品"}
-            className={styles.orderBookItems}
-          />
-          <Col flex="1 1 50px" className={styles.orderTotal}>
-            {currencyFormat(book.price)}{" "}
-          </Col>
-        </List.Item>
-      )}
+      dataSource={orders}
+      rowKey="id"
+      renderItem={(order) => {
+        const { id, items, total, time } = order;
+        const totalAmount = items
+          .map((item) => item.amount)
+          .reduce((prev, curr) => prev + curr);
+        return (
+          <List.Item
+            actions={[
+              <Button
+                type="primary"
+                onClick={() => history.push(`/orders/${id}`)}
+              >
+                查看详情
+              </Button>,
+            ]}
+            className={styles.orderListItem}
+          >
+            <List.Item.Meta
+              avatar={
+                <Space>
+                  {items.slice(0, 2).map((item) => (
+                    <Image
+                      src={item.cover}
+                      width={60}
+                      height={60}
+                      key={item.id}
+                    />
+                  ))}
+                  {items.length > 2 && (
+                    <div className={styles.booksExtra}>+{items.length - 2}</div>
+                  )}
+                </Space>
+              }
+              title={`时间：${new Date(time).toLocaleString()}`}
+              description={`${totalAmount} 件商品`}
+              className={styles.orderBookItems}
+            />
+            <Col flex="1 1 50px" className={styles.orderTotal}>
+              {currencyFormat(total)}{" "}
+            </Col>
+          </List.Item>
+        );
+      }}
       locale={{
         emptyText: (
           <Empty
--- src/routes/Orders/index.js
+++ src/routes/Orders/index.js
@@ -1,5 +1,6 @@
 import books from "@/assets/books";
-import { Col, DatePicker, Divider, Input, Row, Statistic } from "antd";
+import useRequest from "@umijs/use-request";
+import { Col, DatePicker, Divider, Input, Row, Spin, Statistic } from "antd";
 import BookTypeStatistics from "components/BookTypeStatistics";
 import OrderList from "components/OrderList";
 import PageHeader from "components/PageHeader";
@@ -9,6 +10,7 @@ const { RangePicker } = DatePicker;
 
 const Orders = () => {
   const items = books;
+  const { data, loading } = useRequest("/orders/my", { initialData: [] });
 
   return (
     <>
@@ -16,28 +18,35 @@ const Orders = () => {
         <Input.Search placeholder="搜索书籍" />
         <RangePicker />
       </PageHeader>
-      <Row gutter={16} className={styles.statistics}>
-        <Col span={8}>
-          <Statistic title="订单数量" value={items.length} />
-        </Col>
-        <Col span={8}>
-          <Statistic title="商品数量" value={11} />
-        </Col>
-        <Col span={8}>
-          <Statistic title="总金额" prefix={"￥"} value={1128} precision={2} />
-        </Col>
-      </Row>
-      <Row gutter={16}>
-        <Col span={20}>
-          <OrderList books={items} />
-        </Col>
-        <Col>
-          <Divider type="vertical" className={styles.divider} />
-        </Col>
-        <Col span={3}>
-          <BookTypeStatistics />
-        </Col>
-      </Row>
+      <Spin spinning={loading}>
+        <Row gutter={16} className={styles.statistics}>
+          <Col span={8}>
+            <Statistic title="订单数量" value={items.length} />
+          </Col>
+          <Col span={8}>
+            <Statistic title="商品数量" value={11} />
+          </Col>
+          <Col span={8}>
+            <Statistic
+              title="总金额"
+              prefix={"￥"}
+              value={1128}
+              precision={2}
+            />
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={20}>
+            <OrderList orders={data} />
+          </Col>
+          <Col>
+            <Divider type="vertical" className={styles.divider} />
+          </Col>
+          <Col span={3}>
+            <BookTypeStatistics />
+          </Col>
+        </Row>
+      </Spin>
     </>
   );
 };

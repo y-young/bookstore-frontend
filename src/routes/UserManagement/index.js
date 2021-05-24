38af5c9174bcf3cd7686e9f46bfce4b3f29cce3b
Googleplex <yyoung2001@gmail.com>
2021-05-24 17:26:18 +0800

    feat: disable & enable user

--- src/routes/UserManagement/index.js
+++ src/routes/UserManagement/index.js
@@ -1,35 +1,37 @@
+import useRequest from "@umijs/use-request";
 import { Badge, Button, Input, message, Table } from "antd";
 import PageHeader from "components/PageHeader";
 
-const data = [
-  {
-    id: 1,
-    username: "JimGreen",
-    email: "jim@sjtu.edu.cn",
-    books: 879,
-    amount: 3979829,
-    disabled: false,
-  },
-  {
-    id: 2,
-    username: "JohnBrown",
-    email: "john@gmail.com",
-    books: 108,
-    amount: 43323.89,
-    disabled: false,
-  },
-  {
-    id: 3,
-    username: "JoeBlack",
-    email: "joe@outlook.com",
-    disabled: true,
-  },
-];
-
 const UserManagement = () => {
-  const switchDisabled = (user) => {
-    user.disabled = !user.disabled;
-    message.success("操作成功");
+  const { data, loading, refresh } = useRequest("/users");
+  const { run: disableUser } = useRequest(
+    (userId) => ({ method: "post", url: `/users/${userId}/disable` }),
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success("操作成功");
+      },
+    }
+  );
+  const { run: enableUser } = useRequest(
+    (userId) => ({ method: "post", url: `/users/${userId}/enable` }),
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success("操作成功");
+      },
+    }
+  );
+
+  const switchDisabled = async (user) => {
+    const close = message.loading("正在提交...");
+    if (user.disabled) {
+      await enableUser(user.id);
+    } else {
+      await disableUser(user.id);
+    }
+    refresh();
+    close();
   };
 
   const columns = [
@@ -78,7 +80,7 @@ const UserManagement = () => {
     {
       title: "操作",
       key: "action",
-      render: (text, record) => (
+      render: (_, record) => (
         <Button
           type="link"
           danger
@@ -96,7 +98,12 @@ const UserManagement = () => {
       <PageHeader title="用户管理" span={8}>
         <Input.Search placeholder="搜索用户名或电子邮件地址" />
       </PageHeader>
-      <Table columns={columns} dataSource={data} rowKey="id" />;
+      <Table
+        columns={columns}
+        dataSource={data}
+        loading={loading}
+        rowKey="id"
+      />
     </>
   );
 };

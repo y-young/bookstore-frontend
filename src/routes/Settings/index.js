b877de71afd684e130d1963e69a4c6fd7fb200e1
Googleplex <yyoung2001@gmail.com>
2021-06-09 21:21:12 +0800

    feat: update password

--- src/routes/Settings/index.js
+++ src/routes/Settings/index.js
@@ -1,4 +1,6 @@
-import { Button, Card, Col, Form, Input, Row, Typography } from "antd";
+import useRequest from "@umijs/use-request";
+import { Button, Card, Col, Form, Input, message, Row, Typography } from "antd";
+import useAuth from "utils/useAuth";
 
 const layout = {
   labelCol: { span: 6 },
@@ -6,28 +8,36 @@ const layout = {
 };
 
 const Settings = () => {
+  const auth = useAuth();
+  const { user } = auth;
+  const { run, loading } = useRequest(
+    (data) => ({
+      method: "put",
+      url: "/users/password",
+      data,
+    }),
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success("操作成功");
+      },
+    }
+  );
+
   return (
     <Row justify="center">
       <Col span={14}>
         <Card className="singleCard" bordered={false}>
           <Typography.Title level={2}>设置</Typography.Title>
-          <Form name="settings">
-            <Form.Item name="username" label="用户名" {...layout}>
-              Googleplex
+          <Form name="settings" onFinish={run}>
+            <Form.Item label="用户名" {...layout}>
+              {user?.username}
+            </Form.Item>
+            <Form.Item label="电子邮件地址" {...layout}>
+              {user?.email}
             </Form.Item>
             <Form.Item
-              name="email"
-              label="电子邮件地址"
-              rules={[
-                { required: true },
-                { type: "email", message: "请输入有效的电子邮件地址" },
-              ]}
-              {...layout}
-            >
-              <Input type="email" placeholder="电子邮件地址" />
-            </Form.Item>
-            <Form.Item
-              name="newPassword"
+              name="password"
               label="新密码"
               rules={[{ required: true }]}
               {...layout}
@@ -54,7 +64,7 @@ const Settings = () => {
               <Input type="password" placeholder="重复密码" />
             </Form.Item>
             <Form.Item
-              name="password"
+              name="currentPassword"
               label="当前密码"
               rules={[{ required: true }]}
               {...layout}
@@ -62,7 +72,7 @@ const Settings = () => {
               <Input.Password placeholder="当前密码" />
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 2, span: 20 }}>
-              <Button type="primary" htmlType="submit" block>
+              <Button type="primary" htmlType="submit" loading={loading} block>
                 保存
               </Button>
             </Form.Item>

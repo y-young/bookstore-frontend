42e7c0e7cf915bf5657d3615c446dab8222b80b3
Googleplex <yyoung2001@gmail.com>
2021-10-06 20:37:49 +0800

    feat: page view counter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,15 @@
+import useRequest from "@umijs/use-request";
 import { Layout } from "antd";
+import { useHistory } from "react-router-dom";
 import styles from "./index.less";
 
 const Footer = () => {
+  const history = useHistory();
+  const isHome = history.location.pathname === "/";
+  const { data, loading } = useRequest("/statistics/updatePageView", {
+    ready: isHome,
+  });
+
   return (
     <Layout.Footer style={{ textAlign: "center" }}>
       &copy; Copyright 2021{" "}
@@ -13,6 +21,12 @@ const Footer = () => {
       >
         Googleplex
       </a>
+      {isHome && !loading && (
+        <>
+          <br />
+          访问量：{data}
+        </>
+      )}
     </Layout.Footer>
   );
 };
